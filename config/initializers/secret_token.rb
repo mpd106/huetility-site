0498eae4172ad5786801a5afe51e2cae6a473bde
Matt Dickens <mpd106@gmail.com>
2013-05-03 08:55:27 +0100

    Basic rails site, minus test unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HuetilitySite::Application.config.secret_token = 'd279d2c1d097cf9e946e7a27aed6ec6d63f7338ff45c616ba011473ad8668d9088dd05cae53aa8900f6c99dab32619449a2aada9c6cd48a304e694d73bf39cfd'
